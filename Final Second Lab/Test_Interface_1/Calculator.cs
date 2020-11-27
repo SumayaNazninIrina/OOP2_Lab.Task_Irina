11394800464530633a3fff433b11fbea5db41bda
SumayaNazninIrina <72723477+SumayaNazninIrina@users.noreply.github.com>
2020-11-27 15:18:04 +0600

    Test_Interface_1
    Test_Interface_1

---
+++ Final Second Lab/Test_Interface_1/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Interface_1
+{
+    abstract class Calculator
+    {
+
+        public String nm { get; set; }
+        public String power { get; set; }
+
+        public Calculator() { }
+        public Calculator(String nm, String power)
+        {
+            this.nm = nm;
+            this.power = power;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine("The calculator " + nm);
+            Console.WriteLine("The Power of the calculator" + power);
+        }
+    }
+}
+
